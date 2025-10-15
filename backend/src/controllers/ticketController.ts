28c737faf5ffe65f9594d2d0520db97e31faf7ad
Francesco Re s343404 <s343404@studenti.polito.it>
2025-10-15 22:39:24 +0200

    Initial backend ticket local management implementation

--- backend/src/controllers/ticketController.ts
+++ backend/src/controllers/ticketController.ts
@@ -2,6 +2,9 @@ import { Request, Response, NextFunction } from "express";
 import { TicketService } from "../services/TicketService.js";
 import { CreateTicketRequestDTO } from "../dto/ticket.dto.js";
 
+let ticketVector: any[] = []; // In-memory storage for tickets, should use Ticket interface
+let counterTicket: number[] = []; // In-memory storage for the current ticket by the desk
+
 export class TicketController {
 	constructor(private ticketService: TicketService) {}
 
@@ -53,6 +56,8 @@ export class TicketController {
 
 			const ticket = await this.ticketService.createTicket(serviceId);
 
+			ticketVector.push(ticket);
+
 			console.log("Created ticket:", ticket);
 
 			res.status(201).json({
@@ -96,6 +101,65 @@ export class TicketController {
 			next(error);
 		}
 	};
+
+	callNext = async (
+		req: Request,
+		res: Response,
+		next: NextFunction
+	): Promise<void> => {
+		try {
+			//the request filter all the tickets in memory that have a specific service (this is a mocked up version in which there's only one desk that does one service)
+      //For this case we will filter one specific service (service_id=2) and do the filtering based on that. We then take the ticket with the lowest number 
+      //(the one that came first) and we remove it from the in-memory storage.
+      //At the end we return the ticket that was called (Probably just number and service are enough).
+
+      //TODO: implement logic and route part
+
+
+
+
+			// const { desk_Id } = req.params;
+			// const desk_Id = Number(desk_Id);
+
+			// if (!Number.isInteger(desk_Id)) {
+			// 	res.status(400).json({
+			// 		success: false,
+			// 		error: "desk_Id must be an integer",
+			// 	});
+			// 	return;
+			// }
+
+			// const filteredTickets = ticketVector.filter(
+			// 	(ticket) => ticket.service_id === '2'
+			// ).sort((a, b) => a.number - b.number);
+
+      // if (filteredTickets.length > 0) { 
+      //   counterTicket[0] = filteredTickets[0].number; // Update the current ticket for the desk
+      //   ticketVector = ticketVector.filter(ticket => ticket.number !== counterTicket[0]); // Remove the called ticket from the in-memory storage
+      //   res.status(200).json({
+			// 	success: true,
+			// 	data: { currentTicket: counterTicket[0] },
+			//   });
+      // }
+
+		// 	res.status(200).json({
+		// 		success: true,
+		// 		data: filteredTickets,
+			// });
+		} catch (error) {
+			next(error);
+		}
+	};
+
+  checkCurrentTicket = (ticket_id: number) => {
+    //in the real implementation it should check all the desks (for now we have one). If the ticket is in the 
+    //counterTicket vector it means that it is being served, so we can return the desk number, otherwise we return null
+    if(counterTicket[0] === ticket_id) {
+      return { desk: 1 };
+    }
+    return null;
+    //TODO: add the route and fix the implementation part of the vector (to use the ticket interface)
+  };
 
 	callTicket = async (
 		req: Request,
